3459eafcd8fe7ca62eb1e4af4ae437a5ab9f953a
elarth31 <artuhovaela4@gmail.com>
2025-02-28 16:04:03 +0200

    crud

---
+++ src/controllers/contacts.js
@@ -0,0 +1,82 @@
+import createHttpError from 'http-errors';
+import { HTTP_STATUSES } from '../constants/index.js';
+import {
+    createContact,
+    deleteContact,
+    getAllContacts,
+    getContactById,
+    updateContact,
+} from '../services/contacts.js';
+
+const STATUS_OK = HTTP_STATUSES.OK;
+const STATUS_CREATED = HTTP_STATUSES.CREATED;
+const STATUS_NO_CONTENT = HTTP_STATUSES.NO_CONTENT;
+
+export const getContactsController = async (req, res) => {
+    const data = await getAllContacts();
+
+    res.status(STATUS_OK).json({
+    status: STATUS_OK,
+    message: 'Successfully found contacts!',
+    data: data,
+    });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+    return next(createHttpError.NotFound('Contact not found'));
+    }
+
+    res.status(STATUS_OK).json({
+    status: STATUS_OK,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+    });
+};
+
+export const createContactController = async (req, res, next) => {
+    try {
+    const contact = await createContact(req.body);
+    res.status(STATUS_CREATED).json({
+        status: STATUS_CREATED,
+        message: 'Successfully created a contact!',
+        data: contact,
+    });
+    } catch (error) {
+    next(createHttpError.InternalServerError('Failed to create contact'));
+    }
+};
+
+
+export const upsertUserController = async (req, res, next) => {
+    const { contactId } = req.params;
+
+    const result = await updateContact(contactId, req.body);
+
+    if (!result) {
+    return next(createHttpError.NotFound('Contact not found'));
+    }
+
+    const status = result?.isNew ? STATUS_CREATED : STATUS_OK;
+
+    res.status(status).json({
+    status: status,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+    });
+};
+
+export const deleteContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+
+    const contact = await deleteContact(contactId);
+
+    if (!contact) {
+    return next(createHttpError.NotFound('Contact not found'));
+    }
+
+    res.status(STATUS_NO_CONTENT).send();
+};
--- src/server.js
+++ src/server.js
@@ -2,65 +2,36 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
-import { ENV_VARS, HTTP_STATUSES } from './constants/index.js';
+import { ENV_VARS } from './constants/index.js';
 import { corsConfigs } from './config/corsConfigs.js';
 import { pinoConfigs } from './config/pinoConfigs.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(env(ENV_VARS.PORT, 3000));
-const STATUS_OK = HTTP_STATUSES.OK;
-const STATUS_NOT_FOUND = HTTP_STATUSES.NOT_FOUND;
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
+
   app.use(cors(corsConfigs));
+
   app.use(pino(pinoConfigs));
 
-  app.get('/', (req, res) => {
-    res.status(STATUS_OK).json({
-      status: STATUS_OK,
-      message: 'Server is running!',
-    });
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts', async (req, res) => {
-    const data = await getAllContacts();
-    res.status(STATUS_OK).json({
-      status: STATUS_OK,
-      message: 'Successfully found contacts!',
-      data: data,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    console.log('contact: ', contact);
-    if (!contact) {
-      res
-        .status(STATUS_NOT_FOUND)
-        .json({ status: STATUS_NOT_FOUND, message: 'Contact not found' });
-      return;
-    }
-    res.status(STATUS_OK).json({
-      status: STATUS_OK,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(errorHandler);
 
-  app.use((req, res, next) => {
-    const url = req.url;
-    res.status(STATUS_NOT_FOUND).json({
-      status: STATUS_NOT_FOUND,
-      message: `Route ${url} Not found`,
-    });
-  });
+  app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error starting server:', err);
+    process.exit(1);
+  }
+  console.log(`Server is running on PORT: ${PORT}`);
+});
 
- 
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on PORT: ${PORT}`);
-  });
 };
