227dda60b5000905b463f3539075665b40c1d372
elarth31 <artuhovaela4@gmail.com>
2025-03-10 13:43:22 +0200

    date

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -13,16 +13,12 @@ export const validateBody = (schema) => async (req, res, next) => {
       return `${detail.message} (field: ${detail.context.key})`; 
     });
 
-
     console.error(`Validation error: ${errorMessages.join('; ')}`);
 
 
     const httpError = createHttpError(
       HTTP_STATUSES.BAD_REQUEST,
-      'Bad request, the body parameters are incorrect. See details below:',
-      {
-        errors: errorMessages, 
-      }
+      errorMessages.join('; ') 
     );
 
 
