3397e9cf0ad33980a032e98b739a8081b7398981
elarth31 <artuhovaela4@gmail.com>
2025-03-15 17:22:35 +0200

    hw5-auth

--- src/db/models/users.js
+++ src/db/models/users.js
@@ -2,19 +2,19 @@ import { Schema, model } from 'mongoose';
 
 const usersSchema = new Schema(
     {
-    name: { type: String, required: true },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+\@.+\..+/, 'Please enter a valid email address'],
-    },
-    password: { type: String, required: true },
+        name: { type: String, required: true },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+\@.+\..+/, 'Please enter a valid email address'],
+        },
+        password: { type: String, required: true },
     },
     {
-    timestamps: true,
-    versionKey: false,
-},
+        timestamps: true,
+        versionKey: false,
+    }
 );
 
 usersSchema.methods.toJSON = function () {
