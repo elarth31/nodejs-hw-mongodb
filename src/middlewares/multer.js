f0fb65c46b43e5da48fc0fc70e51d33bcd3aa564
elarth31 <artuhovaela4@gmail.com>
2025-03-27 00:45:10 +0200

    img

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,13 +1,21 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
-import { TEMP_UPLOAD_DIR } from '../constants/index.js';
+import { env } from '../utils/env.js';
+import { ENV_VARS } from '../constants/index.js';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, TEMP_UPLOAD_DIR);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, `${uniqueSuffix}_${file.originalname}`);
+cloudinary.config({
+  cloud_name: env(ENV_VARS.CLOUDINARY.CLOUD_NAME),
+  api_key: env(ENV_VARS.CLOUDINARY.API_KEY),
+  api_secret: env(ENV_VARS.CLOUDINARY.API_SECRET),
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'contacts_photos', 
+    allowedFormats: ['jpg', 'jpeg', 'png'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
   },
 });
 
